f265ef26502372168dc719acf1d51c6699588a00
d-wizard <dw.wizard@gmail.com>
2020-08-30 19:11:34 -0500

    Getting started creating algorithm that can easily create/modify a color gradient.

---
+++ colorGradient.cpp
@@ -0,0 +1,164 @@
+/* Copyright 2020 Dan Williams. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <assert.h>
+#include "colorGradient.h"
+
+
+ColorGradient::ColorGradient(size_t numPoints)
+{
+   size_t numZones = numPoints * 2 - 1;
+   float reach = 0.5f / (float)numZones;
+   float deltaBetweenPoints = 1.0f / (float)numPoints;
+
+   m_gradPoints.resize(numPoints);
+   for(auto i = 0; i < m_gradPoints.size(); ++i)
+   {
+      bool first = (i == 0);
+      bool last  = (i == (m_gradPoints.size()-1));
+
+      m_gradPoints[i].hue = 0;
+      m_gradPoints[i].saturation = 0;
+      m_gradPoints[i].lightness = 0;
+      m_gradPoints[i].position = 0.0f + deltaBetweenPoints * i;
+      m_gradPoints[i].reach = (first || last) ? reach * 2.0 : reach;
+   }
+}
+
+ColorGradient::~ColorGradient()
+{
+
+}
+
+void ColorGradient::updateGradient(ColorGradient::eGradientOptions option, float value, int pointIndex)
+{
+   if(pointIndex >= 0 && pointIndex < m_gradPoints.size())
+   {
+      storePrevSettings(option, pointIndex);
+
+      switch(option)
+      {
+         case E_GRAD_HUE:
+            setHue(value, pointIndex);
+         break;
+         case E_GRAD_SATURATION:
+            setSat(value, pointIndex);
+         break;
+         case E_GRAD_LIGHTNESS:
+            setLight(value, pointIndex);
+         break;
+         case E_GRAD_POSITION:
+            setPos(value, pointIndex);
+         break;
+         case E_GRAD_REACH:
+            setReach(value, pointIndex);
+         break;
+         default:
+            assert(0);
+         break;
+      }
+   }
+   else
+   {
+      assert(0);
+   }
+}
+
+
+void ColorGradient::setHue(float value, size_t pointIndex)
+{
+   // Just update hue.
+   m_gradPoints[pointIndex].hue = value;
+}
+
+void ColorGradient::setSat(float value, size_t pointIndex)
+{
+   // Just update saturation.
+   m_gradPoints[pointIndex].saturation = value;
+}
+
+void ColorGradient::setLight(float value, size_t pointIndex)
+{
+   // Just update saturation.
+   m_gradPoints[pointIndex].lightness = value;
+}
+
+void ColorGradient::setPos(float value, size_t pointIndex)
+{
+   if(pointIndex > 0 && pointIndex < (m_gradPoints.size()-1))
+   {
+
+   }
+}
+
+void ColorGradient::setReach(float value, size_t pointIndex)
+{
+
+}
+
+
+void ColorGradient::storePrevSettings(eGradientOptions option, int pointIndex)
+{
+   bool changed = (option != m_previousOption || pointIndex != m_previousIndex);
+   bool newOptionIsLocation = (option == E_GRAD_POSITION || option == E_GRAD_REACH);
+   if(changed && newOptionIsLocation)
+   {
+      m_previousOption = option;
+      m_previousIndex = pointIndex;
+      m_previousGradPoints = m_gradPoints;
+   }
+}
+
+void ColorGradient::locationChanged(size_t pointIndex)
+{
+   bool first = (pointIndex == 0);
+   bool last  = (pointIndex == (m_gradPoints.size()-1));
+
+   bool doLoSide = !first;
+   bool doHiSide = !last;
+
+   if(doLoSide)
+   {
+      // Low
+      float newScalePosLo = m_gradPoints[pointIndex].position - m_gradPoints[pointIndex].reach;
+      float oldScalePosLo = m_previousGradPoints[pointIndex].position - m_previousGradPoints[pointIndex].reach;
+      float ratioLo = newScalePosLo / oldScalePosLo;
+      for(auto i = 0; i < pointIndex; ++i)
+      {
+         m_gradPoints[i].position = m_previousGradPoints[i].position * ratioLo;
+         m_gradPoints[i].reach    = m_previousGradPoints[i].reach    * ratioLo;
+      }
+
+   }
+
+   if(doHiSide)
+   {
+      // High (similar to low side but reflect from +1.0)
+      float newScalePosHi = m_gradPoints[pointIndex].position + m_gradPoints[pointIndex].reach;
+      float oldScalePosHi = m_previousGradPoints[pointIndex].position + m_previousGradPoints[pointIndex].reach;
+      float ratioHi = (FULL_SCALE - newScalePosHi) / (FULL_SCALE - oldScalePosHi);
+      for(auto i = pointIndex+1; i < m_gradPoints.size(); ++i)
+      {
+         float reflectPos = FULL_SCALE - m_previousGradPoints[i].position;
+         m_gradPoints[i].position = FULL_SCALE - (reflectPos * ratioHi);
+         m_gradPoints[i].reach    = m_previousGradPoints[i].reach * ratioHi;
+      }
+   }
+
+}
---
+++ colorGradient.h
@@ -0,0 +1,76 @@
+/* Copyright 2020 Dan Williams. All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <stddef.h>
+#include <vector>
+#include "colorScale.h"
+
+class ColorGradient
+{
+public:
+   static constexpr float FULL_SCALE = 1.0;
+   typedef enum
+   {
+      E_GRAD_HUE,
+      E_GRAD_SATURATION,
+      E_GRAD_LIGHTNESS,
+      E_GRAD_POSITION,
+      E_GRAD_REACH,
+      E_GRAD_INVALID
+   }eGradientOptions;
+
+   ColorGradient(size_t numPoints);
+   virtual ~ColorGradient();
+
+   void updateGradient(eGradientOptions option, float value, int pointIndex);
+
+private:
+   ColorGradient(); // No default constructor.
+
+   static constexpr float MIN_INCREMENT = 0.0078125; // 1/128
+
+   typedef struct
+   {
+      float hue;
+      float saturation;
+      float lightness;
+      float position; // Note: first must be position 0, last must be position 1
+      float reach;
+   }tGradientPoint;
+   
+   std::vector<tGradientPoint> m_gradPoints;
+
+   void setHue(  float value, size_t pointIndex);
+   void setSat(  float value, size_t pointIndex);
+   void setLight(float value, size_t pointIndex);
+   void setPos(  float value, size_t pointIndex);
+   void setReach(float value, size_t pointIndex);
+
+   // Keep track of previous orientation for when position / reach change.
+   eGradientOptions m_previousOption = E_GRAD_INVALID;
+   int m_previousIndex = -1;
+   std::vector<tGradientPoint> m_previousGradPoints;
+   void storePrevSettings(eGradientOptions option, int pointIndex);
+   void locationChanged(size_t pointIndex);
+
+   void scaleLocation(bool lowSide, size_t pointIndex, float ratio);
+
+};
+
