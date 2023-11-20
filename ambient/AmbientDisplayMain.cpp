eccb06e4feeffda6a24d50ef1240a736b6d6c094
d-wizard <dw.wizard@gmail.com>
2023-11-20 02:48:48 +0000

    Add code for displaying a gradient.

--- ambient/AmbientDisplayMain.cpp
+++ ambient/AmbientDisplayMain.cpp
@@ -34,7 +34,10 @@ static std::unique_ptr<SaveRestoreJson> g_saveRestoreJson;
 
 static std::string g_settingsJsonPath = "ambient/AmbientDisplaySettings.json";
 static std::string g_presetJsonPath = "presets.json";
-static int g_presetGradIndex = 0;
+static int g_presetGradIndex = -1;
+
+// Gradient Display
+static bool g_gradDisplay_displayGradient = false;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -53,17 +56,49 @@ static void signalHandler(int signum)
    cleanUpBeforeExit();
    exit(signum); 
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 
 static void parseCmdLineArgs(int argc, char *argv[])
 {
-   // argv[1] is g_presetGradIndex, argv[2] is g_presetJsonPath, argv[3] is g_settingsJsonPath
-   if(argc > 1)
-      g_presetGradIndex = std::stoi(argv[1]);
-   if(argc > 2)
-      g_presetJsonPath = std::string(argv[2]);
-   if(argc > 3)
-      g_settingsJsonPath = std::string(argv[3]);
+   if(argc > 1 && (argv[1][0] == 'd' || (argv[1][0] == 'D')))
+   {
+      g_gradDisplay_displayGradient = true;
+   }
+   else
+   {
+      // argv[1] is g_presetGradIndex, argv[2] is g_presetJsonPath, argv[3] is g_settingsJsonPath
+      if(argc > 1)
+         g_presetGradIndex = std::stoi(argv[1]);
+      if(argc > 2)
+         g_presetJsonPath = std::string(argv[2]);
+      if(argc > 3)
+         g_settingsJsonPath = std::string(argv[3]);
+   }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+static void displayGradient(ColorGradient::tGradient& gradient, size_t numDisplayLEDs, size_t totalLEDs)
+{
+   float brightVal = 0.25;
+   ColorScale::tBrightnessScale brightness = {{brightVal,0},{brightVal,1}};
+   AmbientDisplay gradToRgb(gradient, brightness);
+
+   const size_t leds_total = totalLEDs;
+   const size_t leds_grad = numDisplayLEDs;
+   const size_t leds_right = (leds_total-leds_grad)/2;
+   const size_t leds_left  = (leds_total-leds_grad-leds_right);
+
+   SpecAnLedTypes::tRgbVector rgb;
+   gradToRgb.toRgbVect(leds_grad, rgb, leds_grad);
+   SpecAnLedTypes::tRgbColor black;
+   black.u32 = SpecAnLedTypes::COLOR_BLACK;
+   rgb.insert(rgb.begin(), leds_right, black);
+   rgb.insert(rgb.end(), leds_left, black);
+
+   g_ledStrip->set(rgb);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -98,7 +133,16 @@ int main(int argc, char *argv[])
    g_ledStrip.reset(new LedStrip(DEFAULT_NUM_LEDS, LedStrip::GRB));
    g_ledStrip->clear();
 
-   g_activeAmbient = std::make_unique<AmbDisp3SpotLights>(g_ledStrip, gradient);
+   if(g_gradDisplay_displayGradient)
+   {
+      // Special Mode. Just display the gradient.
+      displayGradient(gradient, unsigned(float(DEFAULT_NUM_LEDS)/4.0), DEFAULT_NUM_LEDS);
+   }
+   else
+   {
+      // Normal Mode.
+      g_activeAmbient = std::make_unique<AmbDisp3SpotLights>(g_ledStrip, gradient);
+   }
 
    /////////////////////////////////////////////////////////////////////////////
    // Main Loop
