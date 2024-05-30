070c120f623993bbb7af29d5816b4512599da11b
d-wizard <dw.wizard@gmail.com>
2024-05-30 02:46:13 +0100

    Change defaults. Speed things up.

--- ambient/AmbientDisplayMain.cpp
+++ ambient/AmbientDisplayMain.cpp
@@ -61,13 +61,13 @@ typedef struct
    float gradSpeedScalar; // 0.1 is a good default
    bool  gradMirror;
 }tAmbGradSettings;
-static const tAmbGradSettings DEFAULT_GRAD_SETTINGS = {0.5, 0.1, true};
+static const tAmbGradSettings DEFAULT_GRAD_SETTINGS = {2.5, 0.3, true};
 static const std::map<std::string, tAmbGradSettings> GRAD_SETTINGS = {
    {"xmas",           DEFAULT_GRAD_SETTINGS},
    {"halloween",      DEFAULT_GRAD_SETTINGS},
    {"merica",         DEFAULT_GRAD_SETTINGS},
    {"rainbow",        {1.0, 0.1, false}},
-   {"rainbow_pastel", {2.0, 1.0, false}},
+   {"rainbow_pastel", {2.0, 0.3, false}},
    {"fire",           DEFAULT_GRAD_SETTINGS},
    {"valentines",     DEFAULT_GRAD_SETTINGS},
    {"st_paddies",     DEFAULT_GRAD_SETTINGS},
